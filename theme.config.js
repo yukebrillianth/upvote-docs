156f4154344603fdc5ee4eab9c6d9abaa3ab0c09
Yuke Brilliant H <61053094+yukebrillianth@users.noreply.github.com>
2021-04-13 14:19:56 +0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,12 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/yukebrillianth/UpVote',
+  docsRepository: 'https://github.com/yukebrillianth/nextjs-docs',
+  titleSuffix: ' – UpVote',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">UpVote</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        Aplikasi Pemilihan Online
       </span>
     </>
   ),
@@ -15,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta name="description" content="UpVote: Aplikasi Pemilihan Online" />
+      <meta name="og:description" content="UpVote: Aplikasi Pemilihan Online" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="twitter:site:domain" content="docs.upvote.morph.my.id" />
+      <meta name="twitter:url" content="https://docs.upvote.morph.my.id" />
+      <meta name="og:title" content="UpVote: Aplikasi Pemilihan Online" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="UpVote" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
@@ -37,5 +38,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
+  footerText: <>MIT {new Date().getFullYear()} © UpVote.</>
 }
